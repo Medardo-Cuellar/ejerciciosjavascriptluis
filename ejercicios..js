a556e27bad470fe97e01b8eda0182b97769b97a7
Luis Medardo <soyriku@gmail.com>
2024-04-18 14:59:01 -0500

    se agrega tercer funcion

--- ejercicios..js
+++ ejercicios..js
@@ -508,69 +508,96 @@ a.	Si:la agrego a mi lista
 */
 
 const getGenres = (movies) => {
-    return movies.reduce((acc, movie) => {
-        if (!acc.includes(movie.genre)) {
-            acc.push(movie.genre);
-        }
-        return acc;
-      },[]);
-}
-
-
-    /* 
-    2. Obtener una lista de los nombres de los directores sin repetir
-        ENTRADA
-        arreglo de objetos
-        SALIDA
-        Arreglo de strings
-        PROCESO
-        1.	Creo un arreglo para mi lista de nombres de directores
-        2.	Escojo el primer objeto
-        3.	Escojo la propiedad director
-        4.	Esta la propiedad ausente de mi lista?
-        a.	Si:la agrego a mi lista
-        5.	Repito hasta terminar mi arreglo
-        6.	Devuelvo el valor de mi lista de directores
-
-    */
-const getDirectors = (movies) => {
-    return movies.reduce((acc, movie) => {
-        if (!acc.includes(movie.director)) {
-            acc.push(movie.director);
-        }
-        return acc;
-        },[]);
-}
-
-    /* 
-   
-   3. Obtener una lista con únicamente los nombres de los actores protagonistas de todas las películas, sin repetir
-   4. Obtener uan lista de clasificaciones (rating) sin repetir
-   5. Obtener una lista de las películas que tienen una duración entre 2 rangos, por ejemplo las películas que duran entre 100 y 120 mins ( los valores de las duraciones deben ser dinámicos)
-   6. Obtener una lista de películas con base en su clasificación
-   7. Obtener una lista de películas películas estrenadas en un rango de años (por ejemplo, entre 2000 - 2010, los valores de los años deben ser dinámicos)
-   8. Obtener una lista de películas con base en el país al que pertenecen
-   9. Obtener una lista de las películas que no obtuvieron premios Óscar
-   10. Obtener la cantidad de películas de cada clasficación. Esta información debe estar organizada de la siguiente forma:
-        {
-            [nombre_de_la_clasificacion]:[cantidad]
-        }
-        Es decir, la propiedad del objeto resultante deber ser la clasificación, y el valor de esa propiedad debe ser la cantidad de películas que pertenecen a esa clasificación
-    11. Obtener la cantidad de películas de cada país, organizada de la siguiente forma:
-        {
-            [pais]:[cantidad]
-        }
-    12. Obtener la edad promedio de los actores protagonistas
-    13. Dado el nombre de un actor, obtener la cantidad de películas de la lista en las que aparece
-   14. Obtener una lista que contenga objetos de cada película con el siguiente formato:
-    
-    {
-        title: "{titulo_de_la_película} - {director} - {duración}",
-        trimmedSynopsis: "{sinopsis}..."
+  return movies.reduce((acc, movie) => {
+    if (!acc.includes(movie.genre)) {
+      acc.push(movie.genre);
     }
+    return acc;
+  }, []);
+}
 
-    **trimmedSynopsis debe estar limitada a 10 palabras, y debe tener puntos suspensivos al final.
+
+/* 
+2. Obtener una lista de los nombres de los directores sin repetir
+    ENTRADA
+    arreglo de objetos
+    SALIDA
+    Arreglo de strings
+    PROCESO
+    1.	Creo un arreglo para mi lista de nombres de directores
+    2.	Escojo el primer objeto
+    3.	Escojo la propiedad director
+    4.	Esta la propiedad ausente de mi lista?
+    a.	Si:la agrego a mi lista
+    5.	Repito hasta terminar mi arreglo
+    6.	Devuelvo el valor de mi lista de directores
+
+*/
+const getDirectors = (movies) => {
+  return movies.reduce((acc, movie) => {
+    if (!acc.includes(movie.director)) {
+      acc.push(movie.director);
+    }
+    return acc;
+  }, []);
+}
+
+/*    3. Obtener una lista con únicamente los nombres de los actores protagonistas de todas las películas, sin repetir
+ENTRADA
+Arreglo de objetos
+SALIDA
+Lista con los nombres de los actores sin repetir
+PROCESO
+
+1.	Hago mi lista de actores, esta vacia porque no hay ningún actor por el momento
+2.	Escojo el primer objeto de mi arreglo
+3.	Escojo el primer objeto del arreglo dentro de la propiedad protagonistas
+4.	¿Este actor no se encuentra dentro de mi lista?
+a.	Lo agrego
+5.	Repito hasta recorrer todo el arreglo
+6.	Devuelvo lista de actores
+
 */
 
 
-console.log(getGenres(movies));
+const getActorNames = (movies) => {
+  return movies.reduce((acc, movie) => {
+    movie.protagonists.forEach((protagonist) => {
+      if (!acc.includes(protagonist.name)) {
+        acc.push(protagonist.name);
+      }
+    });
+    return acc;
+  }, []);
+
+  /* 
+ 
+ 4. Obtener uan lista de clasificaciones (rating) sin repetir
+ 5. Obtener una lista de las películas que tienen una duración entre 2 rangos, por ejemplo las películas que duran entre 100 y 120 mins ( los valores de las duraciones deben ser dinámicos)
+ 6. Obtener una lista de películas con base en su clasificación
+ 7. Obtener una lista de películas películas estrenadas en un rango de años (por ejemplo, entre 2000 - 2010, los valores de los años deben ser dinámicos)
+ 8. Obtener una lista de películas con base en el país al que pertenecen
+ 9. Obtener una lista de las películas que no obtuvieron premios Óscar
+ 10. Obtener la cantidad de películas de cada clasficación. Esta información debe estar organizada de la siguiente forma:
+      {
+          [nombre_de_la_clasificacion]:[cantidad]
+      }
+      Es decir, la propiedad del objeto resultante deber ser la clasificación, y el valor de esa propiedad debe ser la cantidad de películas que pertenecen a esa clasificación
+  11. Obtener la cantidad de películas de cada país, organizada de la siguiente forma:
+      {
+          [pais]:[cantidad]
+      }
+  12. Obtener la edad promedio de los actores protagonistas
+  13. Dado el nombre de un actor, obtener la cantidad de películas de la lista en las que aparece
+ 14. Obtener una lista que contenga objetos de cada película con el siguiente formato:
+  
+  {
+      title: "{titulo_de_la_película} - {director} - {duración}",
+      trimmedSynopsis: "{sinopsis}..."
+  }
+
+  **trimmedSynopsis debe estar limitada a 10 palabras, y debe tener puntos suspensivos al final.
+*/
+
+
+  console.log(getGenres(movies));
