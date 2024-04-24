b05468ba987100c7c365a46cef87334817ce9b75
luis rodriguez <soyriku@gmail.com>
2024-04-23 22:24:46 -0500

    corrected errors

--- ejercicioeventos/eventos/js/main.js
+++ ejercicioeventos/eventos/js/main.js
@@ -1,9 +1,18 @@
-const checkboxes = document.querySelectorAll(".control-checkbox")
+let checkboxes = document.querySelectorAll(".control-checkbox");
+let elements = document.querySelectorAll(".target-element");
 
-checkboxes.forEach((checkbox) => {
-    checkbox.setAttribute("data-target-index", index);
-    checkbox.addEventListener("click", (event) => {
-        console.log(event);
-        console.log(event.target.dataset.targetIndex);
-    })
-})
+checkboxes.forEach((checkbox, index) => {
+  checkbox.setAttribute("data-target-index", index);
+  checkbox.addEventListener("click", (event) => {
+    let elementIndex = event.target.dataset.targetIndex;
+    console.log(event);
+    event.target.checked
+      ? elements[elementIndex].classList.add("d-none")
+      : elements[elementIndex].classList.remove("d-none");
+  });
+});
+
+let test = document.getElementById("test");
+console.log(test);
+console.log(test.dataset);
+console.log(test.dataset.sourceUrl);
